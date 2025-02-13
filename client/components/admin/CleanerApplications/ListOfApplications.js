f3680064543cc5572d92922513b0a44dd9b45d5d
Gkimbo <gavin.a.kimball@gmail.com>
2025-02-13 08:03:38 -0500

    add error messages to form

--- client/components/admin/CleanerApplications/ListOfApplications.js
+++ client/components/admin/CleanerApplications/ListOfApplications.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import homePageStyles from "../../../services/styles/HomePageStyles";
 import FetchData from "../../../services/fetchRequests/fetchData";
+import ApplicationListStyles from "../../../services/styles/ApplicationListStyles"
 
 const ListOfApplications = () => {
   const [listApplications, setApplicationsList] = useState([]);
+  const styles = ApplicationListStyles
 
   useEffect(() => {
     FetchData.getApplicationsFromBackend().then((response) => {
@@ -38,52 +40,6 @@ const ListOfApplications = () => {
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: "30%",
-  },
-  header: {
-    fontSize: 22,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 15,
-  },
-  card: {
-    backgroundColor: "#f8f9fa",
-    padding: 15,
-    marginVertical: 8,
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
-  },
-  name: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  label: {
-    fontSize: 14,
-    fontWeight: "600",
-    marginTop: 4,
-  },
-  value: {
-    fontWeight: "400",
-  },
-  message: {
-    marginTop: 4,
-    fontStyle: "italic",
-    color: "#555",
-  },
-  noData: {
-    textAlign: "center",
-    fontSize: 16,
-    color: "#888",
-  },
-});
 
 export default ListOfApplications;
 
