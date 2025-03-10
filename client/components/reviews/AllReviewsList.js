8eda566c0cde8707f30645aa6aadb19575681f4b
Gkimbo <gavin.a.kimball@gmail.com>
2025-03-10 17:30:40 -0400

    add sort option for all reviews

--- client/components/reviews/AllReviewsList.js
+++ client/components/reviews/AllReviewsList.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Pressable, View, Text, ScrollView, Dimensions } from "react-native";
+import React, { useState, useEffect, useMemo } from "react";
+import { Pressable, View, Text, Dimensions, Picker } from "react-native";
 import { useNavigate } from "react-router-native";
 import homePageStyles from "../../services/styles/HomePageStyles";
 import Icon from "react-native-vector-icons/FontAwesome";
-import topBarStyles from "../../services/styles/TopBarStyles"
+import topBarStyles from "../../services/styles/TopBarStyles";
 import ReviewTile from "./ReviewTile";
 import Review from "../../services/fetchRequests/ReviewClass";
 
 const AllReviewsList = ({ state, dispatch }) => {
   const [allReviews, setAllReviews] = useState([]);
   const [backRedirect, setBackRedirect] = useState(false);
-  const [userId, setUserId] = useState(null);
+  const [sortOption, setSortOption] = useState("dateNewest");
   const { width } = Dimensions.get("window");
   const iconSize = width < 400 ? 12 : width < 800 ? 16 : 20;
   const navigate = useNavigate();
@@ -18,7 +18,7 @@ const AllReviewsList = ({ state, dispatch }) => {
   const fetchReviews = async () => {
     if (state.currentUser.token) {
       const response = await Review.getReviews(state.currentUser.token);
-        setAllReviews(response.reviews);
+      setAllReviews(response.reviews);
     }
   };
 
@@ -54,7 +54,6 @@ const AllReviewsList = ({ state, dispatch }) => {
     return stars;
   };
 
-
   useEffect(() => {
     fetchReviews();
     if (backRedirect) {
@@ -67,9 +66,21 @@ const AllReviewsList = ({ state, dispatch }) => {
     setBackRedirect(true);
   };
 
-  const sortedReviews = allReviews.sort((a, b) => {
-    return new Date(a.createdAt) - new Date(b.createdAt);
-  });
+  const sortedReviews = useMemo(() => {
+    let sorted = [...allReviews];
+
+    if (sortOption === "dateNewest") {
+      sorted.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    } else if (sortOption === "dateOldest") {
+      sorted.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+    } else if (sortOption === "highestRating") {
+      sorted.sort((a, b) => b.review - a.review);
+    } else if (sortOption === "lowestRating") {
+      sorted.sort((a, b) => a.review - b.review);
+    }
+
+    return sorted;
+  }, [allReviews, sortOption]);
 
   const displayReviews = sortedReviews.map((review) => {
     return (
@@ -92,36 +103,42 @@ const AllReviewsList = ({ state, dispatch }) => {
       style={{
         ...homePageStyles.container,
         flexDirection: "column",
-        marginTop: "30%"
+        marginTop: "30%",
       }}
     >
-         <View style={{ alignItems: "center", paddingHorizontal: 16 , marginBottom: "10%"}}>
-          <Text
-            style={{
-              fontSize: 18,
-              fontWeight: "bold",
-              color: "#333",
-              marginBottom: 4,
-            }}
-          >
-            Your Average Rating
-          </Text>
-          
-            <View style={{ flexDirection: "row", marginBottom: 4 }}>
-              {renderStars()}
-            </View>
+      <View
+        style={{
+          alignItems: "center",
+          paddingHorizontal: 16,
+          marginBottom: "10%",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: "bold",
+            color: "#333",
+            marginBottom: 4,
+          }}
+        >
+          Your Average Rating
+        </Text>
 
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "bold",
-              color: "#333",
-              marginBottom: 4,
-            }}
-          >
-            {averageRating.toFixed(1)} / 5.0 ({allReviews.length} Reviews)
-          </Text>
+        <View style={{ flexDirection: "row", marginBottom: 4 }}>
+          {renderStars()}
         </View>
+
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#333",
+            marginBottom: 4,
+          }}
+        >
+          {averageRating.toFixed(1)} / 5.0 ({allReviews.length} Reviews)
+        </Text>
+      </View>
       <View style={homePageStyles.backButtonAllReviewsList}>
         <Pressable
           style={homePageStyles.backButtonForm}
@@ -140,6 +157,30 @@ const AllReviewsList = ({ state, dispatch }) => {
             </View>
           </View>
         </Pressable>
+      </View>
+      <View
+        style={{
+          margin: 5,
+          borderWidth: 1,
+          borderRadius: 5,
+          borderColor: "#ccc",
+        }}
+      >
+        <Picker
+          selectedValue={sortOption}
+          onValueChange={(itemValue) => setSortOption(itemValue)}
+        >
+          <Picker.Item label="Sort by: Date (Newest)" value="dateNewest" />
+          <Picker.Item label="Sort by: Date (Oldest)" value="dateOldest" />
+          <Picker.Item
+            label="Sort by: Rating (Low to High)"
+            value="lowestRating"
+          />
+          <Picker.Item
+            label="Sort by: Rating (High to Low)"
+            value="highestRating"
+          />
+        </Picker>
       </View>
       {displayReviews}
     </View>
